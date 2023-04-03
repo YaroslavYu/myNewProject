4b0e093fb035362f8901db6ccc9fd260d8a62950
YaroslavYu <yaroslavyu801@gmail.com>
2023-04-03 23:26:42 +0300

    add text

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on yourhhhvvs aphaa</Text>
+      <Text>Open up App.js to start working on my phone!!! Hoorey!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
